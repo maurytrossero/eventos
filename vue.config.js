32a0d26f197dc9859eccce31a450e094ac96b29f
Mauricio <maurytrossero@gmail.com>
2025-06-22 19:32:17 -0300

    vue.config2

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,10 @@ const { defineConfig } = require('@vue/cli-service')
 
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: '/'
+  publicPath: '/',
+  configureWebpack: {
+    optimization: {
+      splitChunks: false // 🔥 Esto empaqueta todo en un solo archivo
+    }
+  }
 })
